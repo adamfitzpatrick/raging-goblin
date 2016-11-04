6ca34ee25fc266a29088088ce5d2f3de89e387f1
Adam Fitzpatrick <adam.fitzpatrick@hobsons.com>
2016-11-04 13:13:47 -0700

    Fix s3 deploy buckets

--- deploy_prep.sh
+++ deploy_prep.sh
@@ -3,4 +3,10 @@
 export TAG=$(echo $TRAVIS_BRANCH | grep "^[0-9].[0-9].[0-9]-\?[a-zA-Z]*\.\?[0-9]*$")
 export SEMVER=$(echo $TAG | cut -d - -f 1)
 export ENV_TAG=$(if [ ${#TAG} -gt 0 -a $TAG = $SEMVER ]; then echo prod; else echo $TAG | cut -d - -f 2 | cut -d . -f 1 | grep "qa"; fi)
-export DEPLOY_DIR="raging-goblin-"$ENV_TAG"/"$TAG
+if [ $ENV_TAG ]; then
+    echo "Deploying "$TAG
+    webpack -p --env $ENV_TAG
+    tar -czvf ./$TAG.tar.gz ./public
+    rm -rf ./public/*
+    mv ./$TAG.tar.gz ./public
+fi
